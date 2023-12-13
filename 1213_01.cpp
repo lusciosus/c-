db225bdbbc3c07a35eda1bd7170b7fe9f47477de
lusciosus <153728010+lusciosus@users.noreply.github.com>
2023-12-13 21:51:38 +0900

    Add files via upload

---
+++ 1213_01.cpp
@@ -0,0 +1,10 @@
+//standard input & output
+#include<stdio.h>
+
+// main entry-point
+int main() {
+	printf("저는 옥재호이고 나이는 24이고 학과는 에너지환경학과입니다\n"); 
+	printf("웹 개발을 하기위해서 컴퓨터공부를 하고있습니다\n");
+	printf("백엔드와 프론트엔드를 모두 다룰 수 있는 풀스택 개발자가 되고 싶습니다");
+	return 0;
+}
---
+++ 1213_02.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+
+	printf("%d+%d=%d\n",5,10,15);
+	printf("%d*%d=%d\n",2,3,6);
+	return 0;
+}
